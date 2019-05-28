a5a5139d6108d601d183ffdb4422e55c3b3e6172
Sebiann <redstonetriforce@gmail.com>
2019-05-28 21:52:59 +0200

    making a calculator, made the interface
    Co-authored-by: Austin Albrecht <austin.aa@me.com>

--- index.js
+++ index.js
@@ -1,7 +1,59 @@
-//Load the config from the .env file in (Usage: process.env.VALUE)
-require('dotenv').config()
-//set the api
-const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot(process.env.TOKEN, {polling: true});
+// Load the config from the .env file in (Usage: process.env.VALUE)
+require('dotenv').config();
+// Set the api
+const Telegraf = require('telegraf');
+const Markup = require('telegraf/markup');
+const session = require('telegraf/session')
 
-bot.start();
+const bot = new Telegraf(process.env.TOKEN);
+
+bot.use(session());
+
+// On /start send msg
+bot.start((ctx) => ctx.reply('Hello'));
+// On /help send msg
+bot.help((ctx) => ctx.reply('Help message'));
+
+bot.command('cal', (ctx) => {
+    ctx.session.calcInputs = ''
+    return ctx.reply('Calculator: ', 
+        Markup.inlineKeyboard([
+            [
+                Markup.callbackButton('1', '1-action'),
+                Markup.callbackButton('2', '2-action'),
+                Markup.callbackButton('3', '3-action'),
+                Markup.callbackButton('+', 'plus-action')
+            ],
+            [
+                Markup.callbackButton('4', '4-action'),
+                Markup.callbackButton('5', '5-action'),
+                Markup.callbackButton('6', '6-action'),
+                Markup.callbackButton('-', 'minus-action')
+            ],
+            [
+                Markup.callbackButton('7', '7-action'),
+                Markup.callbackButton('8', '8-action'),
+                Markup.callbackButton('9', '9-action'),
+                Markup.callbackButton('*', 'times-action')
+            ],
+            [
+                Markup.callbackButton('💣', 'del-action'),
+                Markup.callbackButton('0', '0-action'),
+                Markup.callbackButton('=', 'equals-action'),
+                Markup.callbackButton('/', 'divide-action')
+            ]
+        ]).extra()
+    );
+});
+
+bot.action(/[0-9]-action/, (ctx) => {
+    if (!ctx.session.calcInputs) {
+        ctx.session.calcInputs = ''
+    };
+    ctx.session.calcInputs += ctx.match[0].replace('-action', '');
+    console.log('In: ', ctx.session.calcInputs);
+    ctx.answerCbQuery('Ur stoopid');
+})
+
+// Bot start
+bot.launch();
